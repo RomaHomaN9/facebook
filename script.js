0582d2c12ba402a692647847f46ddd58c1a6f044
RomaHomaN9 <Cod_text@ukr.net>
2025-05-30 21:19:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const button = document.querySelector("#button");
+
+const audio = document.getElementById("mySound");
+
 let i = 0;
 
 document.addEventListener("click", function () {
 	document.querySelector("body").style.backgroundImage = "url(./images.jpg)";
+	audio.play();
 	button.style.display = "none";
 });
