e6d0fc2efeae553d396c30fe708e714369eb003d
RomaHomaN9 <Cod_text@ukr.net>
2025-05-30 21:12:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector("#button");
+let i = 0;
+
+document.addEventListener("click", function () {
+	document.querySelector("body").style.backgroundImage = "url(./images.jpg)";
+	button.style.display = "none";
+});
